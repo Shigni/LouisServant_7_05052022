a70ccda0d93b914a31fad6bf0c50bdf9b04cfcca
omar <osmdiop@gmail.com>
2022-11-01 18:24:10 +0000

    Remove unused code

--- scripts/index.js
+++ scripts/index.js
@@ -268,42 +268,6 @@ function searchTag(recipeArray, tag, tagCategory) {
 }
 
 
-
-
-// {
-//     "id": 1,
-//     "name" : "Limonade de Coco",
-//     "servings" : 1,
-//     "ingredients": [
-//         {
-//             "ingredient" : "Lait de coco",
-//             "quantity" : 400,
-//             "unit" : "ml"
-//         },
-//         {
-//             "ingredient" : "Jus de citron",
-//             "quantity" : 2
-//         },
-//         {
-//             "ingredient" : "Crème de coco",
-//             "quantity" : 2,
-//             "unit" : "cuillères à soupe"
-//         },
-//         {
-//             "ingredient" : "Sucre",
-//             "quantity" : 30,
-//             "unit" : "grammes"
-//         },
-//         {
-//             "ingredient": "Glaçons"
-//         }
-//     ],
-//     "time": 10,
-//     "description": "Mettre les glaçons à votre goût dans le blender, ajouter le lait, la crème de coco, le jus de 2 citrons et le sucre. Mixer jusqu'à avoir la consistence désirée",
-//     "appliance": "Blender",
-//     "ustensils": ["cuillère à Soupe", "verres", "presse citron" ]
-// },
-
 function tagCall(color) {
 
     console.log('tagcall appele avec la couleur ', color)
